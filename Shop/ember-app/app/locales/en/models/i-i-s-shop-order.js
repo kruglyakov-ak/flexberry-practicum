092151957b0f0ab95304ba5c8aa8c50884774dc6
Aleksandr Kruglyakov <aleksandr.k.kruglyakov@yandex.ru>
2022-10-28 20:25:23 +0300

    init

---
+++ Shop/ember-app/app/locales/en/models/i-i-s-shop-order.js
@@ -0,0 +1,103 @@
+export default {
+  projections: {
+    OrderE: {
+      status: {
+        __caption__: 'status'
+      },
+      shipmentDate: {
+        __caption__: 'shipmentDate'
+      },
+      paymentDate: {
+        __caption__: 'paymentDate'
+      },
+      totalSum: {
+        __caption__: 'totalSum'
+      },
+      number: {
+        __caption__: 'number'
+      },
+      createDate: {
+        __caption__: 'createDate'
+      },
+      manager: {
+        __caption__: 'manager',
+        lastName: {
+          __caption__: 'lastName'
+        }
+      },
+      orderItem: {
+        __caption__: 'orderItem',
+        amount: {
+          __caption__: 'amount'
+        },
+        priceWTaxes: {
+          __caption__: 'priceWTaxes'
+        },
+        totalSum: {
+          __caption__: 'totalSum'
+        },
+        product: {
+          __caption__: 'product',
+          name: {
+            __caption__: 'name'
+          }
+        }
+      }
+    },
+    OrderL: {
+      status: {
+        __caption__: 'status'
+      },
+      shipmentDate: {
+        __caption__: 'shipmentDate'
+      },
+      paymentDate: {
+        __caption__: 'paymentDate'
+      },
+      totalSum: {
+        __caption__: 'totalSum'
+      },
+      number: {
+        __caption__: 'number'
+      },
+      createDate: {
+        __caption__: 'createDate'
+      },
+      manager: {
+        __caption__: 'manager',
+        lastName: {
+          __caption__: 'lastName'
+        }
+      },
+      orderItem: {
+        __caption__: 'orderItem',
+        amount: {
+          __caption__: 'amount'
+        },
+        priceWTaxes: {
+          __caption__: 'priceWTaxes'
+        }
+      }
+    }
+  },
+  validations: {
+    status: {
+      __caption__: 'status'
+    },
+    shipmentDate: {
+      __caption__: 'shipmentDate'
+    },
+    paymentDate: {
+      __caption__: 'paymentDate'
+    },
+    totalSum: {
+      __caption__: 'totalSum'
+    },
+    manager: {
+      __caption__: 'manager'
+    },
+    orderItem: {
+      __caption__: 'orderItem'
+    }
+  }
+};
