3b3101cd16583d882b2c8b27c7f0bf31c844fe4f
Aleksandr <aleksandr.k.kruglyackov@yandex.ru>
2022-10-31 20:11:00 +0300

    add nameWinitials

---
+++ CodeGen/Shop/ember-app/app/controllers/i-i-s-shop-order-e.js
@@ -0,0 +1,34 @@
+import EditFormController from "ember-flexberry/controllers/edit-form";
+import { set } from "@ember/object";
+
+export default EditFormController.extend({
+  parentRoute: "i-i-s-shop-order-l",
+
+  actions: {
+    configurateOrderItemRow(rowConfig) {
+      let readonlyColumns = ["priceWTaxes", "totalSum"];
+      set(rowConfig, "readonlyColumns", readonlyColumns);
+    },
+  },
+
+  getCellComponent(attr, bindingPath, model) {
+    let cellComponent = this._super(...arguments);
+    if (attr.kind === "belongsTo") {
+      switch (`${model.modelName}+${bindingPath}`) {
+        case "i-i-s-shop-order-item+product":
+          cellComponent.componentProperties = {
+            choose: "showLookupDialog",
+            remove: "removeLookupValue",
+            displayAttributeName: "name",
+            required: false,
+            relationName: "product",
+            projection: "ProductL",
+            autocomplete: true,
+          };
+          break;
+      }
+    }
+
+    return cellComponent;
+  },
+});
---
+++ CodeGen/Shop/ember-app/app/locales/ru/forms/i-i-s-shop-invoice-e.js
@@ -0,0 +1,14 @@
+export default {
+  caption: 'Накладная',
+  'status-caption': 'Статус',
+  'shipmentDateTime-caption': 'Дата и время отгрузки',
+  'totalSum-caption': 'Сумма заказа',
+  'totalWeight-caption': 'Вес заказа (кг)',
+  'note-caption': 'Примечание',
+  'customerName-caption': 'Получатель',
+  'number-caption': 'Номер',
+  'createDate-caption': 'Дата оформления',
+  'order-caption': 'Заказ',
+  'responsiblePerson-caption': 'Товар выдал',
+  'invoiceItem-caption': 'Список товаров к выдаче'
+};
