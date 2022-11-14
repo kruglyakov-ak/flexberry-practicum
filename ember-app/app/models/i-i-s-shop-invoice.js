757f3685eb800b9daf902e2a5a475459e96f32b0
Aleksandr <aleksandr.k.kruglyackov@yandex.ru>
2022-11-15 00:11:48 +0300

    add computed total sum

--- ember-app/app/models/i-i-s-shop-invoice.js
+++ ember-app/app/models/i-i-s-shop-invoice.js
@@ -1,21 +1,47 @@
-import $ from 'jquery';
-import { buildValidations } from 'ember-cp-validations';
+import $ from "jquery";
+import { buildValidations } from "ember-cp-validations";
+import { computed } from "@ember/object";
 
 import {
   defineBaseModel,
   defineProjections,
   ValidationRules,
-  Model as InvoiceMixin
-} from '../mixins/regenerated/models/i-i-s-shop-invoice';
+  Model as InvoiceMixin,
+} from "../mixins/regenerated/models/i-i-s-shop-invoice";
 
-import DocumentModel from './i-i-s-shop-document';
-import { ValidationRules as ParentValidationRules } from '../mixins/regenerated/models/i-i-s-shop-document';
+import DocumentModel from "./i-i-s-shop-document";
+import { ValidationRules as ParentValidationRules } from "../mixins/regenerated/models/i-i-s-shop-document";
 
-const Validations = buildValidations($.extend({}, ParentValidationRules, ValidationRules), {
-  dependentKeys: ['model.i18n.locale'],
-});
+const Validations = buildValidations(
+  $.extend({}, ParentValidationRules, ValidationRules),
+  {
+    dependentKeys: ["model.i18n.locale"],
+  }
+);
 
 let Model = DocumentModel.extend(InvoiceMixin, Validations, {
+  actualTotalWeight: computed("invoiceItem.@each.{weight}", function () {
+    return this.get("invoiceItem").reduce((sum, item) => {
+      const weight = Number(item.get("weight") || 0);
+
+      if (Number.isNaN(weight)) {
+        throw new Error(`Invalid 'weight' for invoice item: '${item}'.`);
+      }
+
+      return sum + weight;
+    }, 0);
+  }),
+
+  actualTotalSum: computed("invoiceItem.@each.{totalSum}", function () {
+    return this.get("invoiceItem").reduce((sum, item) => {
+      const totalSum = Number(item.get("totalSum") || 0);
+      if (Number.isNaN(totalSum)) {
+        throw new Error(`Invalid 'totalSum' for invoice item: '${item}'.`);
+      }
+
+      return sum + totalSum;
+    }, 0);
+  }),
 });
 
 defineBaseModel(Model);
